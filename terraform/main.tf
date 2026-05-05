0e87efc12be3332c08be86da93a7b324c639621e
Mausam <mausamdwivedi2020@gmail.com>
2026-05-05 16:11:25 +0530

    fix terraform state: add s3 remote backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,19 @@
 terraform {
+  required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket  = "mausam-tf-state-2026"
+    key     = "devops/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-  required_version = ">= 1.0"
 }
 
 provider "aws" {
